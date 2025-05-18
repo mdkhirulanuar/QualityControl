8863f5640ed6a99fcd8c6d19ccebdaa284a5740b
Khirul Anuar <mdkhirulanuar@gmail.com>
2025-05-18 10:05:57 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,37 @@
 const CACHE_NAME = 'inspectwisego-cache-v2';
+
 const urlsToCache = [
   './',
   './InspectWiseGo.html',
   './style.css',
-  './script.js',
-  './partsList.js',
   './manifest.json',
   './offline.html',
+
+  // JS modules
+  './main.js',
+  './partsList.js',
+  './aql.js',
+  './validation.js',
+  './photoHandler.js',
+  './reportGenerator.js',
+  './formHandler.js',
+  './verdictHandler.js',
+  './utils.js',
+
+  // Icons
   './icons/icon-192.png',
   './icons/icon-512.png',
+
+  // CDN libraries
   'https://cdnjs.cloudflare.com/ajax/libs/fabric.js/5.2.4/fabric.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
 ];
 
-// Install service worker and pre-cache app shell
+// Install and cache assets
 self.addEventListener('install', event => {
   self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
@@ -28,16 +40,14 @@ self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(cacheNames =>
       Promise.all(
-        cacheNames
-          .filter(name => name !== CACHE_NAME)
-          .map(name => caches.delete(name))
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
       )
     )
   );
   self.clients.claim();
 });
 
-// Intercept requests and serve from cache if offline
+// Fetch handler
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
