84c481d644bc1b7ad360c8638aaa9861286a0b7a
Khirul Anuar <mdkhirulanuar@gmail.com>
2025-05-18 09:07:22 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+// main.js
+window.addEventListener('DOMContentLoaded', function () {
+  // Bind core logic on page load
+  form_bindEvents();
+  verdict_bindEvents();
+  photo_bindEvents();
+  pdf_bindExportButton();
+});
